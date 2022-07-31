de0752d81455f121323e11e6670b5800366829f0
anushreechaudhuri <anuc@mit.edu>
2022-07-31 15:53:28 -0400

    added streamlit as a requirement

--- streamlit/pages/3_📈_Report.py
+++ streamlit/pages/3_📈_Report.py
@@ -33,12 +33,12 @@ level = st.selectbox(
 
 @st.experimental_memo(show_spinner=False, max_entries=1, persist="disk")
 def load_nhpd():
-    return gpd.read_file(f"s3://equity-tool/report/states.geojson")
+    return gpd.read_file(f"s3://equity-tool/report/nhpd.geojson")
 
 
 @st.experimental_memo
 def load_dac():
-    return gpd.read_file(f"s3://equity-tool/report/states.geojson")
+    return gpd.read_file(f"s3://equity-tool/report/dac.geojson")
 
 
 @st.experimental_memo
